feea9f39373f832e179831dfb3d13091b72ebe56
Kassandra <K1995Moreno@gmail.com>
2022-11-02 00:48:52 -0400

    commit tree d6a355f13d0d23dc5b59baf6dfef562ddf65437a

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers= ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+//Return first two from array
+const returnFirstTwoDrivers= (drivers) => drivers.slice(0,2)
+
+//Return last two from array
+const returnLastTwoDrivers= (drivers) => drivers.slice(-2)
+
+//Create new list with functions 
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+//Function in function that multiplies
+function createFareMultiplier(){
+    const multiplier= (a) => a*a
+    return multiplier}
+
+//Double number
+const fareDoubler= (Number) => Number+Number
+
+//Triple number
+const fareTripler= (Number) => Number*3
+
+//Select different drivers
+function selectDifferentDrivers(drivers, selectingDrivers){
+    if (selectingDrivers===returnFirstTwoDrivers){
+        return drivers.slice(0,2)}
+    else if (selectingDrivers===returnLastTwoDrivers){
+        return drivers.slice(-2)}
+}
